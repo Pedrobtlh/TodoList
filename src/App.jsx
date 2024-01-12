7a471f44f8e73f19e7bb0a2ef831b7ebb78112ab
Pedro Henrique Botelho <114683497+Pedrobtlh@users.noreply.github.com>
2024-01-11 21:10:14 -0300

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import TaskInput from "./componentes/TaskInput";
+import TaskLista from "./componentes/TaskLista";
+import styles from "./App.module.css";
+
+//Hooks
+
+import { useState, useEffect } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks") || [])
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = (task) => {
+    //Título, Status e ID
+    setTasks([...tasks, { id: Date.now(), text: task, done: false }]);
+    //LocalStorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  const toggleTaskDone = (taskId) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === taskId ? { ...task, done: !task.done } : task
+      )
+    );
+  };
+
+  const editTask = (taskId) => {};
+
+  return (
+    <div className={styles.container}>
+      <h1>TODO</h1>
+      <TaskInput onAddTask={addTask} />
+      <TaskLista
+        tasks={tasks}
+        onDeleteTask={deleteTask}
+        onToggleTaskDone={toggleTaskDone}
+      />
+    </div>
+  );
+}
+
+export default App;
